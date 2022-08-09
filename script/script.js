27b414170f2d17bd3806b1ca754ce8fd2b0bab59
Valter Duarte <valter.rduarte@gmail.com>
2022-08-09 14:58:19 -0300

    msg user invalid

--- script/script.js
+++ script/script.js
@@ -1,12 +1,23 @@
 function formLogin() {
   const formEmail = document.getElementById('form-email').value
+
   if (formEmail === '') {
+    const pTag = document.createElement('p')
+    const errorMsg = document.createTextNode('Usuário ou senha inválidos')
+    pTag.appendChild(errorMsg)
+
+    const divFormCad = document.getElementsByTagName('label')[1]
+    divFormCad.appendChild(pTag)
+
     return
   }
+
   const formPassword = document.getElementById('password').value
+
   if (formPassword === '') {
     return
   }
+
   localStorage.setItem('nome', formEmail)
   localStorage.setItem('senha', formPassword)
 }

572f9e86e01005bfb3e59340a5de0694a273f07a
Valter Duarte <valter.rduarte@gmail.com>
2022-09-15 14:38:54 -0300

    ÁPI-seiries

--- script/script-catalogo.js
+++ script/script-catalogo.js
@@ -1,20 +1,54 @@
-fetch(
-  'https://api.themoviedb.org/3/trending/movie/week?api_key=ec7b7bc4b19c9ad9b313a59e3ced5dd6&language=pt-BR'
-)
-  .then(async response => {
-    const movies = await response.json()
-    //console.log(movies.results)
-    showSuccess(movies.results)
-  })
-  .catch(error => {
-    alert('Infelizmente ocorreu um erro tente novamente !')
-  })
+const apiKey = 'ec7b7bc4b19c9ad9b313a59e3ced5dd6'
+const baseUrl = 'https://api.themoviedb.org/3'
+const langPtbr = 'language=pt-BR'
+getDataAPI(apiKey, baseUrl, langPtbr)
 
-function showSuccess(results) {
+function getDataAPI(apiKey, baseUrl) {
+  fetch(
+    `${baseUrl}/trending/movie/week?api_key=${baseUrl}&language=${langPtbr}`
+  )
+    .then(async responsePromise => {
+      const responseMovies = await responsePromise.json()
+      const movies = responseMovies.results
+      showSuccess(movies)
+    })
+    .catch(error => {
+      alert('Infelizmente ocorreu um erro tente novamente !')
+    })
+}
+
+function showSuccess(movies) {
   document.getElementById('catalogo').style.display = 'block'
   document.getElementById('initial-loading').style.display = 'none'
 
-  results.forEach(element => {
-    console.log(element.title)
-  })
+  for (let i = 0; i < 3; i++) {
+    console.log(movies[i])
+    console.log(movies[i].poster_path)
+
+    //  tenha que pegar a img do filme
+    //  tenho que pegar a tag img no html
+    //  colocar a img ena div
+  }
+
+  /* movies.forEach(movie => {
+    const movieImg = movie.poster_path
+    const movieDivPosters =
+      document.getElementsByClassName('thumb-films-series')[0]
+    const movieImgTags = movieDivPosters.children
+    console.log(movieImgTags)
+  })*/
+}
+
+function getConfigAPI() {
+  fetch(
+    'https://api.themoviedb.org/3/trending/movie/week?api_key=ec7b7bc4b19c9ad9b313a59e3ced5dd6&language=pt-BR'
+  )
+    .then(async responsePromise => {
+      const responseMovies = await responsePromise.json()
+      const movies = responseMovies.results
+      showSuccess(movies)
+    })
+    .catch(error => {
+      alert('Infelizmente ocorreu um erro tente novamente !')
+    })
 }

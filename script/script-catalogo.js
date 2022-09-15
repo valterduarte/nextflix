b1702416fc5ebc27c6122bc61215130e4f04ea70
Valter Duarte <valter.rduarte@gmail.com>
2022-09-15 16:02:42 -0300

    config-api

--- script/script-catalogo.js
+++ script/script-catalogo.js
@@ -3,10 +3,8 @@ const baseUrl = 'https://api.themoviedb.org/3'
 const langPtbr = 'language=pt-BR'
 getDataAPI(apiKey, baseUrl, langPtbr)
 
-function getDataAPI(apiKey, baseUrl) {
-  fetch(
-    `${baseUrl}/trending/movie/week?api_key=${baseUrl}&language=${langPtbr}`
-  )
+function getDataAPI(apiKey, baseUrl, langPtbr) {
+  fetch(`${baseUrl}/trending/movie/week?api_key=${apiKey}&language=${langPtbr}`)
     .then(async responsePromise => {
       const responseMovies = await responsePromise.json()
       const movies = responseMovies.results
@@ -21,10 +19,11 @@ function showSuccess(movies) {
   document.getElementById('catalogo').style.display = 'block'
   document.getElementById('initial-loading').style.display = 'none'
 
-  for (let i = 0; i < 3; i++) {
-    console.log(movies[i])
-    console.log(movies[i].poster_path)
+  getConfigAPI(apiKey, baseUrl, langPtbr)
 
+  for (let i = 0; i < 3; i++) {
+    //console.log(movies[i])
+    //console.log(movies[i].poster_path)
     //  tenha que pegar a img do filme
     //  tenho que pegar a tag img no html
     //  colocar a img ena div
@@ -40,13 +39,11 @@ function showSuccess(movies) {
 }
 
 function getConfigAPI() {
-  fetch(
-    'https://api.themoviedb.org/3/trending/movie/week?api_key=ec7b7bc4b19c9ad9b313a59e3ced5dd6&language=pt-BR'
-  )
+  fetch(`${baseUrl}/configuration?api_key=${apiKey}`)
     .then(async responsePromise => {
-      const responseMovies = await responsePromise.json()
-      const movies = responseMovies.results
-      showSuccess(movies)
+      const responseAPIConfig = await responsePromise.json()
+      const saveApiConfig = responseAPIConfig
+      console.log(saveApiConfig)
     })
     .catch(error => {
       alert('Infelizmente ocorreu um erro tente novamente !')
